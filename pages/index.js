c08c336827eb42a278938edeea976b18b42425f6
lee <li_yii@163.com>
2024-04-29 17:44:03 +0800

    add database

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,28 @@
-function HomePage() {
-  return <h1>homepage</h1>;
+import EventList from '@/components/events/event-list';
+
+function HomePage(props) {
+  const { FeaturedEvents } = props;
+
+  return <EventList items={FeaturedEvents} />;
 }
 
 export default HomePage;
+
+export async function getStaticProps() {
+  const response = await fetch(
+    'https://nextjs-course-bd5d1-default-rtdb.firebaseio.com/events.json'
+  );
+  const data = await response.json();
+
+  const FeaturedEvents = [];
+  for (let key in data) {
+    if (data[key].isFeatured) {
+      FeaturedEvents.push({ ...data[key], id: key });
+    }
+  }
+  return {
+    props: {
+      FeaturedEvents,
+    },
+  };
+}
