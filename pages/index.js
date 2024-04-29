bdabf750b11359baeef4f8e4a80d9cd5295d19f2
lee <li_yii@163.com>
2024-04-29 17:54:14 +0800

    test revalidate

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import EventList from '@/components/events/event-list';
+import DateIcon from '@/components/icons/date-icon';
 
 function HomePage(props) {
-  const { FeaturedEvents } = props;
+  const { FeaturedEvents, datenow } = props;
 
-  return <EventList items={FeaturedEvents} />;
+  return (
+    <>
+      <EventList items={FeaturedEvents} />
+      <h1>{datenow}</h1>
+    </>
+  );
 }
 
 export default HomePage;
@@ -20,10 +26,24 @@ export async function getStaticProps() {
       FeaturedEvents.push({ ...data[key], id: key });
     }
   }
+
+  const now = new Date();
+
+  // 获取当前的年份、月份、日期、小时、分钟、秒钟
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1; // 月份是从 0 开始计数的，所以要加 1
+  const date = now.getDate();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+
+  const datenow = `当前时间：${year}-${month}-${date} ${hours}:${minutes}:${seconds}`;
+
   return {
     props: {
       FeaturedEvents,
+      datenow,
     },
-    revalidate: 10,
+    revalidate: 60,
   };
 }
